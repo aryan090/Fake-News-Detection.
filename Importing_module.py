630f3b91a1a757d962f0ba5308a363344054b66d
Aryan thakur <91245850+aryan090@users.noreply.github.com>
2022-06-22 11:14:26 +0530

    Update and rename README.md to Importing_module.py

---
+++ Importing_module.py
@@ -0,0 +1,14 @@
+# Fake-News-Detection.
+
+
+## Importing Important libraries
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+import re
+import string
